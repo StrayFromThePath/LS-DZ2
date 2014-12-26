05b189b206534fd2ade64a8ff1f61fcf91b87d59
unknown <tas23alex@mail.ru>
2014-12-26 19:35:13 +0300

    Пофиксил main.js(select)

--- js/main.js
+++ js/main.js
@@ -1,45 +1,40 @@
 $(document).ready(function(){
     // Select
-    $('.slct').click(function(){
+    $('.slct').click(function(e){
+        e.preventDefault();
 
-        /* Заносим выпадающий список в переменную */
         var dropBlock = $(this).parent().find('.drop');
 
-
-        /* Делаем проверку: Если выпадающий блок скрыт то делаем его видимым*/
+        // Делаем проверку: Если выпадающий блок скрыт то делаем его видимым
         if( dropBlock.is(':hidden') ) {
             dropBlock.slideDown();
 
-            /* Выделяем ссылку открывающую select */
+            // Выделяем ссылку открывающую select
             $(this).addClass('active');
 
-            /* Работаем с событием клика по элементам выпадающего списка */
-            $('.drop__item').click(function(){
-
-                /* Заносим в переменную HTML код элемента
-                 списка по которому кликнули */
+            //Работаем с событием клика по элементам выпадающего списка
+            $('.drop__item').click(function(e){
+                e.preventDefault();
+                //Заносим в переменную HTML код элемента
+                // списка по которому кликнули
                 var selectResult = $(this).text();
 
-                /* Находим наш скрытый инпут и передаем в него
-                 значение из переменной selectResult */
-                $(this).parent().parent().find('input').val(selectResult);
+                //Находим наш скрытый инпут и передаем в него
+                // значение из переменной selectResult
+                $(this).parent().parent().find('input').val(selectResult).addClass('active');
 
-                /* Передаем значение переменной selectResult в ссылку которая
-                 открывает наш выпадающий список и удаляем активность */
+                // Передаем значение переменной selectResult в ссылку которая
+                // открывает наш выпадающий список и удаляем активность
                 $(this).parent().parent().find('.slct').removeClass('active').text(selectResult);
 
-                /* Скрываем выпадающий блок */
+                //Скрываем выпадающий блок
                 dropBlock.slideUp();
             });
-
-            /* Продолжаем проверку: Если выпадающий блок не скрыт то скрываем его */
+            // Продолжаем проверку: Если выпадающий блок не скрыт то скрываем его
         } else {
             $(this).removeClass('active');
             dropBlock.slideUp();
         }
-
-        /* Предотвращаем обычное поведение ссылки при клике */
-        return false;
     });
 
     $('.menu__trigger').on('click', function(e) {
@@ -48,27 +43,45 @@ $(document).ready(function(){
         var $this = $(this),
             item = $this.closest('.menu__item'),
             list = $this.closest('.menu__list'),
+            link = $this.closest('.menu__trigger'),
             items = list.find('.menu__item'),
+            links = items.find('.menu__trigger'),
             content = item.find('.menu-drop'),
             otherContent = list.find('.menu-drop'),
             duration = 300;
 
+
         if (!item.hasClass('active')) {
             items.removeClass('active');
             item.addClass('active');
-
 
             otherContent.stop(true,true).slideUp(duration);
             content.stop(true,true).slideDown(duration);
         } else {
             content.stop(true,true).slideUp(duration);
             item.removeClass('active');
-
         }
 
-
-
+        //присваиваем класс, подсвечивающий активную ссылку синим
+        if (!link.hasClass('active')) {
+            links.removeClass('active');
+            link.addClass('active');
+        } else {
+            link.removeClass('active');
+        }
     });
 
+    $('.drop__link-grid').on('click', function(){
+        var $this = $(this);
 
+        $this.addClass('disabled').attr('disabled','disabled');
+        $('.drop__link-lines').removeClass('disabled').removeAttr('disabled','disabled');
+    });
+
+    $('.drop__link-lines').on('click', function(){
+        var $this = $(this);
+
+        $this.addClass('disabled').attr('disabled','disabled');
+        $('.drop__link-grid').removeClass('disabled').removeAttr('disabled','disabled');
+    });
 });
